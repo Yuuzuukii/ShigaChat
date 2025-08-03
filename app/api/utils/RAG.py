a8e12bc635039bb5d0965a49e7375be373584763
Ryo Yamauchi <is0695fv@ed.ritsumei.ac.jp>
2025-08-03 10:28:38 +0900

    ごめん戻した

--- app/api/utils/RAG.py
+++ app/api/utils/RAG.py
@@ -11,12 +11,7 @@ from tqdm import tqdm
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import HumanMessage
 from pathlib import Path
-
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../..")))
-
-from app.config import DATABASE #パスを修正
+from config import DATABASE #パスを修正
 
 
 dotenv.load_dotenv()
